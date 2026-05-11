b2e2bf4e7daefb6b1be3302500823ab0264b2466
Компьютер <@DESKTOP-KA92S2A>
2026-05-11 17:48:14 +0500

    restore

---
+++ InteractiveTWPF/Converters/ScoreDisplayConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace InteractiveTWPF.Converters
+{
+    public class ScoreDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int score)
+            {
+                return $"{score} баллов";
+            }
+            return value?.ToString() ?? "0 баллов";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
